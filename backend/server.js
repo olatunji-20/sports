102b0ee140cbdb353dca36d1e42976f65f08501a
falana sheriff <falanasheriffdeen@gmail.com>
2023-04-13 09:18:23 +0100

    adding backend to project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("BACKEND RUNNING!!!!!")
+});
+
+app.listen(5000, () => {
+    console.log("server running on port 5000.")
+})
