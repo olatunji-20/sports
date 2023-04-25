c76bd6ac83b98341e67a2f46f3d1f40fbd3d23c9
falana sheriff <falanasheriffdeen@gmail.com>
2023-04-25 17:07:22 +0100

    add product image

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,17 @@ app.post("/", (req, res) => {
     try {
         console.log(req.body)
         console.log(req.files.picture.name)
+
+        const productImage = req.files.picture;
+        
+        const productName = req.body.productName;
+        const maker = req.body.maker;
+        const productPrice = req.body.productPrice;
+        const discount = req.body.discount;
+        const series = req.body.series;
+
+        console.log(productImage.name + " " + productName + " " + maker + " " + productPrice + " " + discount + " " + series);
+
     }catch(err) {
         console.log(err)
     }
