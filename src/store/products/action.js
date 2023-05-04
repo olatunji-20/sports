c07ae8d66840c0d0907423a9617baf137a88c05b
falana sheriff <falanasheriffdeen@gmail.com>
2023-05-04 13:02:45 +0100

    adding render api to lovesport vuex action

--- src/store/products/action.js
+++ src/store/products/action.js
@@ -3,8 +3,10 @@ import axios from "axios"
 
 export async function getProducts({ commit }) {
     // let url = "http://localhost:5000/all-products"
-    let url = "https://my-json-server.typicode.com/olatunji-20/lovesports/sales-product"
+    // let url = "https://my-json-server.typicode.com/olatunji-20/lovesports/sales-product"
     // let url = "http://localhost:5000/sales-product"
+
+    let url = "https://lovesports-api.onrender.com/all-products"
     await axios.get(url).then((response) => {
         try {
             let result = response.data;
@@ -19,8 +21,10 @@ export async function getProducts({ commit }) {
 
 export async function getProducts2({ commit }) {
     // let url = "http://localhost:5000/all-products"
-    let url = "https://my-json-server.typicode.com/olatunji-20/lovesports/latest-products"
+    // let url = "https://my-json-server.typicode.com/olatunji-20/lovesports/latest-products"
     // let url = "http://localhost:5000/latest-products"
+    
+    let url = "https://lovesports-api.onrender.com/all-products"
     await axios.get(url).then((response) => {
         let result = response.data;
         commit("setProducts2", result)
@@ -30,8 +34,10 @@ export async function getProducts2({ commit }) {
 
 export async function getProduct({ commit }, id) {
     // let url = `http://localhost:5000/one-product/${id}`
-    let url = `https://my-json-server.typicode.com/olatunji-20/lovesports/sales-product/${id}`
+    // let url = `https://my-json-server.typicode.com/olatunji-20/lovesports/sales-product/${id}`
     // let url = `http://localhost:5000/sales-product/${id}`
+    
+    let url = `https://lovesports-api.onrender.com/one-product/${id}`
     await axios.get(url).then((response) => {
         let result = response.data
         commit("setProduct", result)
@@ -42,8 +48,10 @@ export async function getProduct({ commit }, id) {
 
 export async function getProduct2({ commit }, id) {
     // let url = `http://localhost:5000/one-product/${id}`
-    let url = `https://my-json-server.typicode.com/olatunji-20/lovesports/latest-products/${id}`
+    // let url = `https://my-json-server.typicode.com/olatunji-20/lovesports/latest-products/${id}`
     // let url = `http://localhost:5000/latest-products/${id}`
+    
+    let url = `https://lovesports-api.onrender.com/one-product/${id}`
     await axios.get(url).then((response) => {
         let result = response.data
         commit("setProduct", result)
@@ -63,7 +71,8 @@ export async function uploadProduct({ }, productDetails) {
     formData.append("picture", productDetails.selectedImage, productDetails.selectedImage.name);
 
     // let url = "http://localhost:5000/"
-    let url ="https://my-json-server.typicode.com/olatunji-20/lovesports/sales-product"
+    let url = "https://lovesports-api.onrender.com/all-products"
+    // let url ="https://my-json-server.typicode.com/olatunji-20/lovesports/sales-product"
     try {
         await axios.post(url, formData)
     } catch (error) {
