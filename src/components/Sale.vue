11911cd389b235134983b1be137028417e4fffb7
falana sheriff <falanasheriffdeen@gmail.com>
2023-05-06 17:14:24 +0100

    finesse for sale image link

--- src/components/Sale.vue
+++ src/components/Sale.vue
@@ -6,7 +6,8 @@
                 <div v-for="product in products" :key="product.id" class="board">
                     <div class="item">
                         <!-- <img :src="product.imageUrl" :alt="product.productName" class="prod" /> -->
-                        <img :src="'https://lovesports-api.onrender.com/product-images/' + product.productName + product.imageName" :alt="product.productName" class="prod" />
+                        <!-- <img :src="'https://lovesports-api.onrender.com/product-images/' + product.productName + product.imageName" :alt="product.productName" class="prod" /> -->
+                        <img :src="'https://raw.githubusercontent.com/olatunji-20/lovesport-backend/master/product-images/' + product.imageName" :alt="product.productName" class="prod" />
                         <div v-if="product.discount" class="discount">-{{ product.discount }}%</div>
                         <div class="love"><img src="../../public/assets/love.png" class="luv" /></div>
                         <div class="free"><img class="motor" src="../../public/assets/motor.png" />
